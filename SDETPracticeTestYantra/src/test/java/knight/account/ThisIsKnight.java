1ad68da9fbe995d915a9269eaea648df6636a314
Dell <Dell@Thippeswamy>
2024-04-18 18:03:49 +0530

    second commit

--- SDETPracticeTestYantra/src/test/java/knight/account/ThisIsKnight.java
+++ SDETPracticeTestYantra/src/test/java/knight/account/ThisIsKnight.java
@@ -5,6 +5,11 @@ public class ThisIsKnight {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is from another knight account");
+		
+		int a= 10;
+		int b= 20;
+		int c=a+b;
+		System.out.println(a+"+"+b+"-->"+c);
 	}
 
 }
